d35eb5685822f45e3fa55eb0685829de180eebba
Maria Geller <maria.geller2@gmail.com>
2015-11-21 18:28:12 -0500

    Update project.

--- scripts/build.clj
+++ scripts/build.clj
@@ -3,9 +3,10 @@
 (println "Building ...")
 
 (let [start (System/nanoTime)]
-  (b/build (b/inputs "src" "src2")
+  (b/build (b/inputs "src")
     {:main 'cljs-1437-test.core
      :output-to "out/cljs_1437_test.js"
      :output-dir "out"
+     :optimizations :whitespace
      :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
--- src/cljs_1437_test/core.cljs
+++ src/cljs_1437_test/core.cljs
@@ -1,10 +1,8 @@
 (ns cljs-1437-test.core
   (:require [cljs.js :as cljs]
-            [cljs-1437-test.foo :as foo]
-            [cljs-1437-test.foo2 :as foo2]))
+            [cljs-1437-test.foo :as foo]))
 
 (enable-console-print!)
 
 (println "Hello world!")
 (println (:foo (meta #'foo/foo)))
-(println (:foo2 (meta #'foo2/foo2)))
